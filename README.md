3e61ff86ca7d402cbd609d930b9a1c738209e64f
Daniel Abishek <daniel.abishek@softsuave.org>
2024-03-16 19:24:20 +0530

    pass by value

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # goLang
+
+# Go is a pass-by-value language
+* Go makes "copies" of values when passed into functions
+
+# Non-pointer values
+* Strings
+* Ints
+* Floats
+* Booleans
+* Arrays
+* Structs
+
+#Pointer Wrapper Value
+* Slices
+* Maps
+* Functions
--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+func assignValueInMap(mapData map[string]float64, key string, value float64) {
+	mapData[key] = value
+}
+
+func assignValueInString(getValue string) {
+	getValue = "Abishek"
+}
+
 func main() {
 	menu := map[string]float64{
 		"Plan Dosa":   20,
@@ -11,15 +19,20 @@ func main() {
 		"Kal Dosa":    20,
 		"Masala Dosa": 40,
 	}
+	name := "Daniel"
 
 	fmt.Println(menu)
 	fmt.Println(menu["Nice Dosa"])
-	menu["Egg Dosa"] = 45
-
+	assignValueInMap(menu, "Egg Dosa", 45)
+	assignValueInString(name)
 	// looping the maps
 	for key, value := range menu {
 		fmt.Println(key, "- Rs:", value,"/-")
 	}
+
+	fmt.Println(name) // the name will not change because it pass the copy of the parent value, Output: Daniel
+	fmt.Println(menu) // The menu will change because the map pass it's pointer value(memory address) Output: map[Egg Dosa:45 Kal Dosa:20 Masala Dosa:40 Nice Dosa:25 Plan Dosa:20]
+
 
 
 }
